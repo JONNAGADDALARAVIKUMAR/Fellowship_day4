3636ad74a54c22a8ef5cc31f7378df0520786771
JONNAGADDALA RAVIKUMAR <ravikumarjit583@gmail.com>
2020-10-28 09:45:33 +0530

    Updare contact

--- AddressBook.java
+++ AddressBook.java
@@ -5,24 +5,26 @@ import java.util.Scanner;
 
 public class AddressBook extends ContactPerson
 {
+	static ArrayList<String> addressBook = new ArrayList<String>();
+
 
 	public static void main(String[] args) 
 	{
 		
-		int choice,yes =1, no=2, noOfContacts,mailPosition=7,PhNoPosition=6;
+		int choice,noOfContacts,mailPosition=7,PhNoPosition=6,one=1,two=2;
 		String details = null;
+		final int editContact=2, exit=3;
 		
 		Scanner scan = new Scanner(System.in);
-		ArrayList<String> AddressBook = new ArrayList<String>();
 		
 		System.out.print("\n\n\tWelcome to Addres Book Programme\n\nCreate an Address\n\n");
 
 		for(noOfContacts=1;true;noOfContacts++) 
 		{
 			
-			if(noOfContacts > no)//Works from 3rd iteration. i.e checks 2nd iteration details in third iteration and continues
+			if(noOfContacts > two)//Works from 3rd iteration. i.e checks 2nd iteration details in third iteration and continues
 			{
-				for(String contactString : AddressBook)
+				for(String contactString : addressBook)
 				{
 				
 					String[] address = contactString.split("  ");
@@ -34,45 +36,85 @@ public class AddressBook extends ContactPerson
 						
 						choice=scan.nextInt();
 					
-						if(choice == no)
+						if(choice == exit)
 							break;
 					}
 					else
 					{
-						AddressBook.add(details);
+						addressBook.add(details);
 
-						System.out.print("\n\nContact Added Successfully\n\n\nCreate Another Contact\n\t1.Yes\n\t2.No\n\nChoice : ");
+						System.out.print("\n\nContact Added Successfully\n\n\n1.Create Another Contact\n\t2.Edit Contact\n\t2.Exit\n\nChoice : ");
 						choice=scan.nextInt();
 					
-						if(choice == no)
+						switch (choice)
+						{
+						case editContact:
+							editDetails();
+							System.out.println("Edited Successfully");
 							break;
+						case exit:
+							break;
+						}
 					}
 				}
 			}
 			details = personDetails();//Takes data from first iteration
 			
 			//It adds details to the address book only once(first time)
-			if(noOfContacts == yes)
+			if(noOfContacts == one)
 			{
-				AddressBook.add(details);
-				details=null;
-				System.out.print("\nContact Added Successfully\n\n\nCreate Another Contact\n\n\t1.Yes\n\t2.No\n\nChoice : ");
+				addressBook.add(details);
+	
+				System.out.print("\nContact Added Successfully\n\n\n1.Create Another Contact\n\n\t2.Edit contact\n\t3.Exit\n\nchoice : ");
 				
 				choice=scan.nextInt();
-				if(choice == no)
+				switch (choice)
+				{
+				case editContact:
+					for(String address : addressBook)
+					{
+						String[] contact = address.split("  ");
+						
+						System.out.println(contact[0]+","+contact[1]+","+contact[6]+","+contact[7]);
+					}
+					editDetails();
+					System.out.println("Edited Successfully");
 					break;
+				case exit:
+					break;
+				}
 			}
 		}
-		System.out.println("\nAddress Book Closed");
 	}
+	
+private static void editDetails() {
+		
+		System.out.print("Enter Mobile Number OR Email : ");
+		editWith=scan.next();
+		int i=0;
+		for(String address : addressBook)
+		{
+			if(address.contains(editWith))
+			{
+				editedDetails=personDetails();//it gets updated address
+				
+				addressBook.set(i, editedDetails);//replaces the updated address
+			}
+			i++;
+		}		
+	}
+
+	
 }
 
 class ContactPerson {
 	
-	static String firstName, lastName, city, zip, email, phoneNumber, state, address;
+	static String firstName, lastName, city, zip, email, phoneNumber, state, address,editedDetails,editWith;
 	
 	static Scanner scan = new Scanner(System.in);
- 
+	
+	
+	
 	public static String personDetails()
 	{
 		
